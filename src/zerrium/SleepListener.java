91536e41094fb825fc9b7228c37660a8a51c5032
zerrium <willysusilo1@gmail.com>
2020-12-15 15:56:11 +0700

    Refactored code to Spigot API 1.12.2

--- src/zerrium/SleepListener.java
+++ src/zerrium/SleepListener.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World.Environment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
@@ -15,8 +16,6 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.world.TimeSkipEvent;
-import org.bukkit.event.world.TimeSkipEvent.SkipReason;
 
 /**
  *
@@ -27,11 +26,12 @@ public class SleepListener implements Listener{
     
     @EventHandler
     public void onPlayerSleep(PlayerBedEnterEvent event){
-        if(event.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)){
-            counter++;
-            Bukkit.broadcastMessage(ChatColor.GOLD+"[SleepNotify] "+ChatColor.RESET+event.getPlayer().getName()+
-                    " is sleeping! "+ChatColor.AQUA+"["+counter+"/"+getRequired()+"]");
-        }
+        int required = getRequired();
+        long time = event.getPlayer().getWorld().getTime();
+        if(event.isCancelled()) return;
+        counter++;
+        Bukkit.broadcastMessage(ChatColor.GOLD+"[SleepNotify] "+ChatColor.RESET+event.getPlayer().getName()+
+                " is sleeping! "+ChatColor.AQUA+"["+counter+"/"+required+"]");
     }
     
     @EventHandler
@@ -46,14 +46,10 @@ public class SleepListener implements Listener{
     @EventHandler
     public void onNightSkipped(TimeSkipEvent event){
         counter = 0;
-        SkipReason sr = event.getSkipReason();
-        if(sr.equals(SkipReason.NIGHT_SKIP)){ //skipped time by vanilla
+        if(event.getWorld().getTime() == 0)
             Bukkit.broadcastMessage(ChatColor.GOLD+"[SleepNotify] "+ChatColor.RESET+"Night has been skipped. It's morning!");
-        }else if(sr.equals(SkipReason.CUSTOM) && event.getWorld().getTime()==0){ //skipped time by plugin
-            Bukkit.broadcastMessage(ChatColor.GOLD+"[SleepNotify] "+ChatColor.RESET+"Night has been skipped. It's morning!");
-        }
     }
-    
+
     @EventHandler
     public void onPlayerChangeDimension(PlayerChangedWorldEvent event){
         if(counter != 0){
